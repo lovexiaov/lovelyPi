fde2ccc475f73fe9d7d93cd14c5ef37f206d7e16
dev4love <cantywei@gmail.com>
2017-01-11 18:37:55 +0800

    Update baidu_tts demo, add function to get tts from fanyi,baidu.com

--- src/demo/baidu_tts.py
+++ src/demo/baidu_tts.py
@@ -4,6 +4,7 @@ import requests
 import urllib.parse
 
 URL_TTS = u'http://tts.baidu.com/text2audio?'
+URL_TTS2 = u'http://fanyi.baidu.com/gettts?'
 TARGET = r'../../res/out.mp3'
 
 headers = {
@@ -32,5 +33,28 @@ def tts(text, lan='zh', sped='5'):
         f.write(response.content)
 
 
+def tts2(text, lan='zh', sped='5'):
+    """
+    Convert text to MP3 audio with Baidu's TTS engine.
+    :param text:
+    :param lan: What language to be convert. Support zh(Chinese) and en(English)
+    :param sped: Sound Speed
+    :return:
+    """
+
+    session = requests.Session()
+    session.get('http://fanyi.baidu.com/')
+    data = {
+        u'lan': lan,
+        u'spd': sped,
+        u'text': text,
+        u'source': u'web'
+    }
+    url = URL_TTS2 + urllib.parse.urlencode(data)
+    response = session.get(url, headers=headers)
+    with open(TARGET, 'wb') as f:
+        f.write(response.content)
+
+
 if __name__ == '__main__':
-    tts(u'这是一个中文测试文本。我就想看看有句号你会停顿不！！！')
+    tts2(u'这是一个中文测试文本')
